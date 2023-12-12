cf5116a38d657388e039631f27adfab9927464f4
Mohammad Imam Hossain <37970985+devimam@users.noreply.github.com>
2023-12-12 23:37:30 +0600

    Add files via upload

---
+++ Codeforce 1850A To My Critics.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for(int i=0;i<t;i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+
+        if(a+b>=10 || a+c>=10 || b+c>=10) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
