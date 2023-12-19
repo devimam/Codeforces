cf6fe5c4a9e02f4237e215f977526dd066c67d57
Mohammad Imam Hossain <37970985+devimam@users.noreply.github.com>
2023-12-19 17:28:35 +0600

    Add files via upload

---
+++ Codeforce 1186A Vus the Cossack and a Contest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,m,k;
+    cin>>n>>m>>k;
+
+    if(m>=n && k>=n) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+    return 0;
+}
