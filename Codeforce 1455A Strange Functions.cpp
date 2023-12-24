5a91e2762e7b7cc5299c24642663bda64440e825
Mohammad Imam Hossain <37970985+devimam@users.noreply.github.com>
2023-12-25 00:27:33 +0600

    Add files via upload

---
+++ Codeforce 1455A Strange Functions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for(int i=0;i<t;i++){
+        string s;
+        cin>>s;
+
+        cout<<s.size()<<endl;
+    }
+
+    return 0;
+}
