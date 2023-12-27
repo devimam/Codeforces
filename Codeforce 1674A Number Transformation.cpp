b4c77aa4f58239dff2bf03d5f6b5cdcb9c82a6fa
Mohammad Imam Hossain <37970985+devimam@users.noreply.github.com>
2023-12-27 19:32:46 +0600

    Add files via upload

---
+++ Codeforce 1674A Number Transformation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for(int i=0;i<t;i++){
+        int x,y;
+        cin>>x>>y;
+
+        if(y%x!=0) cout<<0<<" "<<0<<endl;
+        else{
+            cout<<1<<" "<<y/x<<endl;
+        }
+    }
+
+    return 0;
+}
