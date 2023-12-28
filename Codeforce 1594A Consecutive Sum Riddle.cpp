2eacdcd9bb98f363d2fca4b2f1c0e68628ee6996
Mohammad Imam Hossain <37970985+devimam@users.noreply.github.com>
2023-12-28 19:08:05 +0600

    Add files via upload

---
+++ Codeforce 1594A Consecutive Sum Riddle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    for(int i=0;i<t;i++){
+        long long int n;
+        cin>>n;
+
+        cout<<-1*(n-1)<<" "<<n<<endl;
+
+    }
+
+    return 0;
+}
